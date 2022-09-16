4b7cc73f3133b86f592b59eeb23f9f22829015dc
Harish <harish.chanemougam@3wa.io>
2022-09-16 16:44:27 +0200

    ButtonsScript

--- Assets/Script/EntityInteraction.cs
+++ Assets/Script/EntityInteraction.cs
@@ -1,17 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class EntityInteraction : MonoBehaviour
 {
+    [SerializeField] InputActionReference _clickInput;
+
+    [SerializeField] TextMeshProUGUI _focusName;
+    [SerializeField] Image _cursor;
+
+    [SerializeField] Sprite _emptyCursor;
+    [SerializeField] Color _emptyColor;
+
+    [SerializeField] Sprite _lockedCursor;
+    [SerializeField] Color _lockedColor;
+
     private void Update()
     {
-        Debug.DrawRay(transform.position, transform.forward*2f);
-        if(Physics.Raycast(transform.position, transform.forward, out RaycastHit hitInfo, 2f))
+        Debug.DrawRay(transform.position, transform.forward * 2f);
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hitInfo, 2f))
         {
+            if (hitInfo.collider.TryGetComponent<IUsable>(out var RedButton))
+            {
+                RedButton.use();
+            }
 
+            else if (hitInfo.collider.TryGetComponent<IUsable>(out var GreenButton))
+            {
+                GreenButton.use();
+            }
         }
     }
 }
--- Assets/Script/RedButton.cs
+++ Assets/Script/RedButton.cs
@@ -1,12 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.WSA;
 
 public class RedButton : MonoBehaviour
 {
+    GameObject gameobject;
+    TextMeshProUGUI _text;
+    Renderer _renderText;
+    bool _canvas;
+    
+    public void LaunchPanCarte()
+    {
+        _canvas.gameobject.Setactive(true);
+        _renderText.text = _text;
+
+
+        confimation.Input.action.actionMap.Enable();
+        confimation.Input.action.started += waitForInput;
+    }
+
+    void waitForInput(InputAction.CallBackContext obj)
+    {
+        _canvas.gameobject.Setactive(false);
+        _renderText.text = "";
+
+        confimation.Input.asset.Disable();
+        confimation.Input.asset.started -= waitForInput;
+
+        onPancartStop?.Invoke();
+
+    }
     public string GetName()
     {
         return "RedButton";
+        
     }
 
     public void Use()
