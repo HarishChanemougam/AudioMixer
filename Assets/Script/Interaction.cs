1fc6e46dbbc3413a960bf1f12a0b0658ed59e089
Harish <harish.chanemougam@3wa.io>
2022-09-16 11:29:33 +0200

    InitialCommit

---
+++ Assets/Script/Interaction.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    private void Update()
+    {
+        Debug.DrawRay(transform.position, transform.forward, Color.red);
+
+        var myLayerMask = LayerMask.GetMask("Joueur", "Default");
+
+       if(Physics.Raycast(transform.position, transform.forward, out var hitInfo, 2f))
+        {
+
+        }
+    }
+}
